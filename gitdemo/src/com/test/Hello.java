859d493607ffdeeca59c14ba582ffccf9427c937
luo5 <justin200209@live.cn>
2013-03-30 00:07:47 +0800

    测试提交到远程。

--- gitdemo/src/com/test/Hello.java
+++ gitdemo/src/com/test/Hello.java
@@ -3,6 +3,7 @@ package com.test;
 public class Hello {
 	private int id;
 	private String name;
+	private String hell0;
 
 	public int getId() {
 		return id;
@@ -18,6 +19,14 @@ public class Hello {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getHell0() {
+		return hell0;
+	}
+
+	public void setHell0(String hell0) {
+		this.hell0 = hell0;
 	}
 
 }
